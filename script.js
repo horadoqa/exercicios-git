15d67ec1658d877617c15d1b4dc4a709251c18ba
Ricardo Fahham <rfahham@hotmail.com>
2025-01-24 16:09:08 -0300

    Adicionado função para o botão

---
+++ script.js
@@ -0,0 +1,7 @@
+// Obtém o botão com id "myButton"
+const button = document.getElementById('myButton');
+
+// Adiciona um evento de clique ao botão
+button.addEventListener('click', function() {
+    alert('Você clicou no botão!');
+});
